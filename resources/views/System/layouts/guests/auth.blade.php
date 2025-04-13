7c4ca2987708217c940f316c19d0cf07c9e982d9
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2025-04-12 20:57:32 -0500

    Update auth.blade.php

--- resources/views/System/layouts/guests/auth.blade.php
+++ resources/views/System/layouts/guests/auth.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 </div>
                 <div class="d-flex col-12 col-lg-4 align-items-center authentication-bg p-sm-12 p-6 bg-white">
-                    <div class="w-px-400 mx-auto mt-12 pt-5">
+                    <div class="w-px-400 mx-auto mt-12 pt-5 mb-3">
                         {{ $slot }}
                     </div>
                 </div>
