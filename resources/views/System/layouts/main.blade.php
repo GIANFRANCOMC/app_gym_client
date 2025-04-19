abf909f33355c8fe76844521916468eefa271191
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2025-04-18 23:30:54 -0500

    Branding

--- resources/views/System/layouts/main.blade.php
+++ resources/views/System/layouts/main.blade.php
@@ -25,21 +25,23 @@
         <div class="layout-wrapper layout-content-navbar">
             <div class="layout-container">
                 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-                    <div class="app-brand demo d-flex justify-content-center mt-3">
+                    {{-- <div class="app-brand demo d-flex justify-content-center mt-3">
                         <a href="#" class="app-brand-link">
                             <image src="{{ asset('System/assets/img/utils/admin/app_navbar.png') }}" width="160px" height="45px"/>
                         </a>
-                    </div>
+                    </div> --}}
 
                     <div class="menu-inner-shadow"></div>
 
-                    <ul class="menu-inner py-1">
-                        <li class="menu-header mb-4">
+                    <ul class="menu-inner my-3">
+                        <li class="menu-header mb-3">
                             <span class="menu-header-text" data-i18n="Profile">
                                 <div class="row">
                                     <div class="col col-auto">
                                         <div class="avatar avatar-online mt-2">
-                                            <span class="avatar-initial rounded-circle bg-label-success">{{ substr($user->name, 0, 2) }}</span>
+                                            <span class="avatar-initial rounded-circle bg-label-dark">
+                                                <image src="{{ asset('System/assets/img/utils/admin/app_header.png') }}"/>
+                                            </span>
                                         </div>
                                     </div>
                                     <div class="col col-auto">
@@ -97,20 +99,27 @@
                     </ul>
                 </aside>
                 <div class="layout-page">
-                    <nav
-                        class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-                        id="layout-navbar">
+                    <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
                         <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
                             <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                            <i class="ti ti-menu-2 ti-sm"></i>
+                                <i class="ti ti-menu-2 ti-sm"></i>
                             </a>
                         </div>
                         <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                            <div class="navbar-nav align-items-center">
+                                <div class="nav-item navbar-search-wrapper mb-0">
+                                    <a class="nav-item nav-link search-toggler d-flex align-items-center px-0" href="javascript:void(0);">
+                                        <image src="{{ asset('System/assets/img/utils/admin/app_navbar.png') }}" width="160px" height="45px"/>
+                                    </a>
+                                </div>
+                            </div>
                             <ul class="navbar-nav flex-row align-items-center ms-auto">
                                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                     <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                         <div class="avatar avatar-online">
-                                            <span class="avatar-initial rounded-circle bg-label-success">{{ substr($user->name, 0, 2) }}</span>
+                                            <span class="avatar-initial rounded-circle bg-label-dark">
+                                                <image src="{{ asset('System/assets/img/utils/admin/app_header.png') }}"/>
+                                            </span>
                                         </div>
                                     </a>
                                     <ul class="dropdown-menu dropdown-menu-end">
@@ -119,7 +128,9 @@
                                         <div class="d-flex">
                                             <div class="flex-shrink-0 me-3">
                                                 <div class="avatar avatar-online">
-                                                    <span class="avatar-initial rounded-circle bg-label-success">{{ substr($user->name, 0, 2) }}</span>
+                                                    <span class="avatar-initial rounded-circle bg-label-dark">
+                                                        <image src="{{ asset('System/assets/img/utils/admin/app_header.png') }}"/>
+                                                    </span>
                                                 </div>
                                             </div>
                                             <div class="flex-grow-1">
