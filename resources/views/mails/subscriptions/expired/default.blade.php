fea3b46d23864a47faa34b25e7a6cf1798fec444
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2025-04-30 12:34:13 -0500

    Update default.blade.php

--- resources/views/mails/subscriptions/expired/default.blade.php
+++ resources/views/mails/subscriptions/expired/default.blade.php
@@ -13,12 +13,12 @@
     <table width="100%" cellpadding="0" cellspacing="0">
         <tr>
             <td align="center" style="padding: 40px 0; background: {{ $ownerApp->color_palette->primary }};">
-                <small style="color: {{ $ownerApp->color_palette->text_by_primary }};">
+                <div style="display: inline-block; vertical-align: middle; color: {{ $ownerApp->color_palette->text_by_primary }}; font-size: 14px;">
                     {{ $company->commercial_name }} by
-                    <a href="{{ $ownerApp->web }}" style="color: {{ $ownerApp->color_palette->text_by_primary }}; text-decoration: underline;">
-                        <img src="{{ asset($ownerApp->assets->img->logotype) }}" alt="Logo" width="150" style="margin-bottom: 10px;">
-                    </a>
-                </small>
+                </div>
+                <a href="{{ $ownerApp->web }}" style="display: inline-block; vertical-align: middle; margin-left: 8px;">
+                    <img src="{{ asset($ownerApp->assets->img->logotype) }}" alt="Logo" width="100" style="vertical-align: middle; display: inline-block;"/>
+                </a>
                 <h1 style="color: {{ $ownerApp->color_palette->text_by_primary }}; font-size: 24px; margin: 10px 0 0;">¡Te extrañamos, {{ $customer->name }}!</h1>
             </td>
         </tr>
